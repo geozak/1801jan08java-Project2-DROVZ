7886e6e1fcbf228f2cd2456d4b316fc320d0ca51
Zakary Stone <zakarystone@gmail.com>
2018-02-28 09:13:13 -0500

    something

--- src/main/java/com/revature/project2/MainDriver.java
+++ src/main/java/com/revature/project2/MainDriver.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project2MainDriver {
+public class MainDriver {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project2MainDriver.class, args);
+		SpringApplication.run(MainDriver.class, args);
 	}
 
 }
