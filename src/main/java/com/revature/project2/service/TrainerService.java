0d9caadf5a6fc3fcf664259c3f295a0c05b5f3d0
Oscar5273 <oscarvasquez17@gmail.com>
2018-03-02 10:27:17 -0500

    Trainer Controller

---
+++ src/main/java/com/revature/project2/service/TrainerService.java
@@ -0,0 +1,12 @@
+package com.revature.project2.service;
+
+import java.util.List;
+
+import com.revature.project2.model.Trainer;
+
+public interface TrainerService {
+	
+	public List<Trainer> getAllTrainers();
+	public Trainer getTrainerByUrl(String url);
+
+}
